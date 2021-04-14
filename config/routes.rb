0bdefe79e0ea08fd1ff95339571add3c58d2c6d5
nqt <quytruong@viemprire.com>
2021-04-14 16:58:19 +0700

    edit user

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
       root "dashboard#index"
     end
     
-    devise_for :users
+    devise_for :users 
+    # devise_scope :users do
+    #   get "/u", to: "devise/sessions#index", as: :new_user_session
+    # end
+    # get "/u" => "devise#index"
   #  devise_for :users
   #  root "static_pages#home"
 end
