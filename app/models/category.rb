88745875fbdd05c1475c023f45ecca85c194cbb4
nqt <quytruong@viemprire.com>
2021-04-23 14:35:35 +0700

    update category

--- app/models/category.rb
+++ app/models/category.rb
@@ -7,7 +7,15 @@ class Category < ApplicationRecord
     
     default_scope { order(position: :asc) }
     
-    has_attached_file :avatar, styles: { medium: "100x100>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+    has_attached_file :avatar, styles: { medium: "300x150#" }
     
     validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+
+    def default_avatar
+        avatar.url(:medium)
+    end
+
 end
+
+
