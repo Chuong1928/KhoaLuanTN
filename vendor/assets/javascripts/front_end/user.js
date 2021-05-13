e8468adaee60b35bc9c6f8531311da367a6fd61e
Chuong1928 <6789tyh@gmail.com>
2021-05-14 00:23:26 +0700

    add hover info use

---
+++ vendor/assets/javascripts/front_end/user.js
@@ -0,0 +1,15 @@
+// $(function(){
+//     // Enables popover
+//     $(".example-popover").popover({
+        
+//         html : true, 
+//         content: function() {
+//           return $(".example-popover-content").html();
+//         },
+//         title: function() {
+//           return $(".example-popover-title").html();
+//         }
+//     });
+// });
+
+
